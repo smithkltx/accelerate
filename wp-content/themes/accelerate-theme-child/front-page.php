4f1637c8cc61e30e8bb738762c9891e204af76d4
Kristen L. Smith <smithkltx@gmail.com>
2017-07-03 15:29:47 -0500

    New front page case studies

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -25,6 +25,35 @@ get_header(); ?>
 	</div><!-- .container -->
 </section><!-- .home-page -->
 
+<section class="featured-work">
+	<div class="site-content">
+		<h5>Featured Work</h5>
+
+		<ul class="homepage-featured-work">
+		<?php query_posts('posts_per_page=3&post_type=case_studies'); ?>
+		 <?php while ( have_posts() ) : the_post();
+		 		$image_1 = get_field("image_1");
+				$image_2 = get_field("image_2");
+				$image_3 = get_field("image_3");
+				$size = "medium";
+		 ?>
+		 	<li class="individual-featured-work">
+		 		<figure>
+					<?php echo wp_get_attachment_image($image_1, $size); ?>
+					<?php echo wp_get_attachment_image($image_2, $size); ?>
+					<?php echo wp_get_attachment_image($image_3, $size); ?>
+				</figure>
+
+			 <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+		 	</li>
+		 <?php endwhile; // end of the loop. ?>
+		<?php wp_reset_query(); //resets the altered query back to the original ?>
+		</ul>
+
+	</div>
+
+</section>
+
 <section class="recent-posts">
  <div class="site-content">
   <div class="blog-post">
